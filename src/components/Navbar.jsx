d6f825d32489eec996db2de4c3587ebc1b55688b
anshaymishra29 <anshay966@gmail.com>
2024-06-28 15:34:26 +0530

    Adding navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,51 @@
+import React from 'react';
 import { Link } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import './Navbar.css';
 
 function Navbar() {
   return (
-    <nav className="bg-white shadow-md p-4">
-      <div className="container mx-auto flex justify-between items-center">
-        <div className="flex items-center">
-          <Link to="/" className="text-xl font-bold">
-            <img src="/path/to/logo.png" alt="Logo" className="h-8 w-8 mr-2 inline-block" />
-            Home
-          </Link>
-        </div>
-        <div className="flex space-x-4">
-          <Link to="/howitworks" className="text-gray-600 hover:text-gray-900">How It Works</Link>
-          <Link to="/borrowers" className="text-gray-600 hover:text-gray-900">Borrowers</Link>
-          <Link to="/blogs" className="text-gray-600 hover:text-gray-900">Blogs</Link>
-          <Link to="/investors" className="text-gray-600 hover:text-gray-900">Investors</Link>
-          <Link to="/login" className="text-gray-600 hover:text-gray-900">Login</Link>
-          <Link to="/signup" className="text-gray-600 hover:text-gray-900">Signup</Link>
+    <nav className="navbar navbar-expand-lg bg-light fixed-top" style={{ position: "absolute"}}>
+      <div className="container-fluid d-flex justify-content-end">
+        <button className="btn btn-outline-primary" style={{ backgroundColor: "#47c9af", color: "white", marginTop: 20, height: 80, width: 170, fontSize: 20, marginLeft: 100 }}>
+          <span style={{ textAlign: 'center' }}><strong>Lending<br />Buddha</strong></span>
+        </button>
+
+        <div className="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <Link className="nav-link active" aria-current="page" to="/">Home</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link active" aria-current="page" to="/howitworks">HowItWorks</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link active" aria-current="page" to="/borrowers">Borrowers</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link active" aria-current="page" to="/blogs">Blogs</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link active" aria-current="page" to="/investors">Investors</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link active" aria-current="page" to="/aboutus">AboutUs</Link>
+            </li>
+            <li className="nav-item dropdown">
+              <a className="nav-link active dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                <strong>SignUp</strong>
+              </a>
+              <ul className="dropdown-menu">
+                <li><Link className="dropdown-item" to="/signup/lender">Lender</Link></li>
+                <li><Link className="dropdown-item" to="/signup/borrower">Borrower</Link></li>
+              </ul>
+            </li>
+            
+            <li className="nav-item">
+              <Link to="/login" className="btn btn-outline-light" style={{ background: "transparent", border: "solid", color: "black" }} type="button"><strong>Login</strong></Link>
+            </li>
+          </ul>
         </div>
       </div>
     </nav>
