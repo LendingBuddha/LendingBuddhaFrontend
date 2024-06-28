b555df1b763d88d2065e40636fe92342ec417d13
Harsh Verma <128226098+cse2020BT@users.noreply.github.com>
2024-06-28 15:11:50 +0530

    updated login page

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -3,43 +3,60 @@ import '../login/login.css'
 function Login() {
   return (
 
-    <div class="limiter">
-    <div class="container-login100">
-        <div class="wrap-login100">
-            <div class="login100-pic js-tilt" data-tilt>
-                <img src="{{ url_for('static',filename='') }}" alt="image" />
+    <div className="limiter">
+    <div className="container-login100">
+        <div className="wrap-login100">
+            <div className="login100-pic js-tilt" data-tilt>
+                <img src="{{ url_for('static',filename='#') }}" alt="image" />
             </div>
 
-            <form class="login100-form validate-form" method="POST">
-                <span class="login100-form-title">
+            <form className="login100-form validate-form" method="POST">
+                <span className="login100-form-title">
 					LOG IN
 				</span>
+              
 
-                <div class="wrap-input100 validate-input" data-validate="Valid email is required: ex@abc.xyz">
-                    <input class="input100" type="text" name="uname" id="uname" placeholder="Username" />
-                    <span class="focus-input100"></span>
-                    <span class="symbol-input100">
+
+{/* Email Address */}
+                
+                <div className="wrap-input100 validate-input" data-validate="Valid email is required: ex@abc.xyz">
+                    <input className="input100" type="email" name="email" id="email" placeholder="Email" />
+                    <span className="focus-input100"></span>
+                    <span className="symbol-input100">
 						<i class="fa fa-envelope" aria-hidden="true"></i>
 					</span>
                 </div>
 
-                <div class="wrap-input100 validate-input" data-validate="Password is required">
-                    <input class="input100" type="password" name="passw" id="passw" placeholder="Password" />
-                    <span class="focus-input100"></span>
-                    <span class="symbol-input100">
+{/* password */}
+
+                <div className="wrap-input100 validate-input" data-validate="Password is required">
+                    <input className="input100" type="password" name="passw" id="passw" placeholder="Password" />
+                    <span className="focus-input100"></span>
+                    <span className="symbol-input100">
 						<i class="fa fa-lock" aria-hidden="true"></i>
 					</span>
                 </div>
 
-                <div class="container-login100-form-btn">
-                    <button type="submit" class="login100-form-btn">
+{/* login button */}
+                
+                <div className="container-login100-form-btn">
+                    <button type="submit" className="login100-form-btn">
 						Login
 					</button>
                 </div>
 
-                <div class="text-center p-t-90">
+{/* forget password */}
+<div class="container-forget100-form-btn">
+                    <button type="submit" className="forget100-form-btn">
+						Forget Password
+					</button>
+                </div>
+
+{/* Register account */}
+
+                <div className="text-center p-t-90">
                     Don't have an account?
-                    <a class="txt3" href="register">
+                    <a className="txt3" href="#">
 						Register now.
 					</a>
                 </div>
