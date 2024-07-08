6b8e20f76b7374b06899d7a5cb87e2157588842b
Manoj <manojssankarlingam01@gmail.com>
2024-07-08 18:14:18 +0530

    save

---
+++ src/components/BorrowerPanel/demoData.jsx
@@ -0,0 +1,11 @@
+export const demoTransactions = [
+    { id: 1, amountBorrowed: 1000, amountRepaid: 500, date: '2023-01-10' },
+    { id: 2, amountBorrowed: 2000, amountRepaid: 1500, date: '2023-02-15' },
+    { id: 3, amountBorrowed: 1500, amountRepaid: 1500, date: '2023-03-20' },
+  ];
+  
+  export const demoBorrowingSummary = {
+    currentBorrowings: 4500,
+    amountAbleToBorrow: 5000,
+  };
+  
