009cee96fdf49740cc2189ebbf2f45656975d436
Arnav Goutam <2021eo3ar@mitsgwl.ac.in>
2024-07-08 18:59:43 +0530

    buid : layout, dashboard, overviewChart and BorrowerList

--- src/App.jsx
+++ src/App.jsx
@@ -10,26 +10,33 @@ import Signup from './pages/signup/Signup';
 import AboutUs from './pages/aboutUs/AboutUs';
 import './App.css';
 import Footer from './components/Footer';
-
+import Layout from './pages/layout/Layout';
+import Dashboard from './pages/dashboard/Dashboard';
 function App() {
   return (
-    <Router>
-      <div>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/HowItWorks" element={<HowItWorks />} />
-          <Route path="/borrowers" element={<Borrowers />} />
-          <Route path="/blogs" element={<Blogs />} />
-          <Route path="/investors" element={<Investors />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/AboutUs" element={<AboutUs/>} />
+    // <Router>
+    //   <div>
+    //     <Navbar />
+    //     <Routes>
+    //       <Route path="/" element={<Home />} />
+    //       <Route path="/HowItWorks" element={<HowItWorks />} />
+    //       <Route path="/borrowers" element={<Borrowers />} />
+    //       <Route path="/blogs" element={<Blogs />} />
+    //       <Route path="/investors" element={<Investors />} />
+    //       <Route path="/login" element={<Login />} />
+    //       <Route path="/signup" element={<Signup />} />
+    //       <Route path="/AboutUs" element={<AboutUs/>} />
 
-        </Routes>
-        <Footer />
-      </div>
-    </Router>
+    //     </Routes>
+    //     <Footer />
+    //   </div>
+    // </Router>
+
+    <div className="App">
+    <Layout>
+      <Dashboard />
+    </Layout>
+  </div>
   );
 }
 
---
+++ src/pages/dashboard/Dashboard.jsx
@@ -0,0 +1,42 @@
+// src/components/Dashboard.js
+// import RecentSales from './RecentSales';
+import React from 'react';
+import './dashboard.css';
+import BorrowerList from "../BorrowersList/BorrowerList"
+import OverviewChart from '../overviewChart/overviewChart';
+
+const Dashboard = () => {
+  return (
+    <div className="dashboard">
+      <div className="card">
+        <h3>Total money Lended</h3>
+        <h2>$45,231.89</h2>
+        <p>+20.1% from last month</p>
+      </div>
+      <div className="card">
+        <h3>Earned Profit</h3>
+        <h2>$2350</h2>
+        <p>+180.1% from last month</p>
+      </div>
+      <div className="card">
+        <h3>Sales</h3>
+        <h2>+12,234</h2>
+        <p>+19% from last month</p>
+      </div>
+      <div className="card">
+        <h3>Active Deals Now</h3>
+        <h2>+57</h2>
+        <p>+20 since last hour</p>
+      </div>
+      <div className="overview">
+        <h3>Overview</h3>
+       <OverviewChart/>
+      </div>
+      <div className="recent-sales">
+       <BorrowerList/>
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard;
