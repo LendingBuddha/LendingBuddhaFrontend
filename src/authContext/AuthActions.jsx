ffbe7e9b97fe5f6b07f94c8b6cc4e84cffca8482
Arnav Goutam <2021eo3ar@mitsgwl.ac.in>
2024-07-10 18:57:54 +0530

    refactor : navbar.jsx, auth actions.jsx, authcontext.jsx and login.jsx

--- src/authContext/AuthActions.jsx
+++ src/authContext/AuthActions.jsx
@@ -1,13 +1,16 @@
 export const loginStart = () => ({
-    type: "LOGIN_START",
-  });
-  export const loginSuccess = (user) => ({
-    type: "LOGIN_SUCCESS",
-    payload: user,
-  });
-  export const loginFailure = () => ({
-    type: "LOGIN_FAILURE",
-  });
+  type: "LOGIN_START",
+});
+
+export const loginSuccess = (user) => ({
+  type: "LOGIN_SUCCESS",
+  payload: user,
+});
+
+export const loginFailure = () => ({
+  type: "LOGIN_FAILURE",
+});
+
   
   //logout
   
