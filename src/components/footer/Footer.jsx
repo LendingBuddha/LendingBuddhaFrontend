e602a2161a62177fea697a457387c02a9b444ec9
sunnydowari <86121971+sunnydowari@users.noreply.github.com>
2024-07-12 18:50:40 +0530

    BUILD: home, footer, signup, about us updated.

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -23,37 +23,33 @@ const Footer = () => {
                     <div className="footer-section">
                         <ul>
                         <h4>Lenders</h4>
-                            <li><a href="#home">Home</a></li>
-                            <li><a href="/HowItWorks">How it works</a></li>
-                            <li><a href="#about-us">About us</a></li>
-                            <li><a href="#team">Team</a></li>
-                            <li><a href="#advisory">Advisory</a></li>
-                            <li><a href="/AboutUs">Blog</a></li>
-                            <li><a href="#calculate-emi">Calculate EMI</a></li>
+                            <li><a href="#home">why invest?</a></li>
+                            <li><a href="/HowItWorks">Investors elegibility</a></li>
+                            <li><a href="#about-us">How to invest?</a></li>
+                            <li><a href="#team">Returns and fees</a></li>
+                            <li><a href="#advisory">Investors code of conduct</a></li>
+                            <li><a href="/AboutUs">Investors FAQ</a></li>
+                            <li><a href="#calculate-emi">Tutorial for Investors</a></li>
                         </ul>
                     </div>
                     <div className="footer-section">
                         <ul>
                         <h4>Borrowers</h4>
-                            <li><a href="#home">Home</a></li>
-                            <li><a href="#how-it-works">How it works</a></li>
-                            <li><a href="#about-us">About us</a></li>
-                            <li><a href="#team">Team</a></li>
-                            <li><a href="#advisory">Advisory</a></li>
-                            <li><a href="#blogs">Blog</a></li>
-                            <li><a href="#calculate-emi">Calculate EMI</a></li>
+                            <li><a href="#home">Why borrow?</a></li>
+                            <li><a href="#how-it-works">Borrowers elegibility</a></li>
+                            <li><a href="#about-us">How to create effective listing</a></li>
+                            <li><a href="#team">Purpose of loan</a></li>
+                            <li><a href="#advisory">Repayment and interest rates</a></li>
+                            <li><a href="#blogs">Borrowers FAQ</a></li>
+                            <li><a href="#calculate-emi">Tutorial for beginners</a></li>
                         </ul>
                     </div>
                     <div className="footer-section">
                         <ul>
                         <h4>Legal</h4>
-                            <li><a href="#home">Home</a></li>
-                            <li><a href="#how-it-works">How it works</a></li>
-                            <li><a href="#about-us">About us</a></li>
-                            <li><a href="#team">Team</a></li>
-                            <li><a href="#advisory">Advisory</a></li>
-                            <li><a href="#blogs">Blog</a></li>
-                            <li><a href="#calculate-emi">Calculate EMI</a></li>
+                            <li><a href="#home">Terms of use</a></li>
+                            <li><a href="#how-it-works">Privay policy</a></li>
+                            <li><a href="#about-us">Refund and Cancellation</a></li>
                         </ul>
                     </div>
                     <div className="footer-subscribe">
@@ -70,15 +66,15 @@ const Footer = () => {
                     </div>
                 </div>
                 <div className="footer-contact">
-                    <p><Email /> contactus@gmail.com</p>
-                    <p><Phone /> +91 - 34567890</p>
-                    <p><Room /> Address: Lending Buddha, H-183, Sector 63 Noida, 201301</p>
+                    <p><Email />contactus@getlendingbuddha.online</p>
+                    <p><Phone /> +91-7060372107</p>
+                    <p><Room />Lending Buddha, C/O We Work H-183 , Sector 63 Noida, 201301</p>
                 </div>
             </div>
             
         </footer>
         <div className="footer-bottom">
-                <p>Copyright @ getLendingBuddha 2024</p>
+                <p>Copyright@getLendingBuddha 2024</p>
             </div>
             </>
     );
